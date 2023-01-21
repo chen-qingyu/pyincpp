196ccae59c5fe7f2b3754fe5bcec03c04f23b9a6
chen-qingyu <236521827@qq.com>
2023-01-21 20:30:51 +0800

    add Integer

---
+++ Sources/Integer.hpp
@@ -0,0 +1,28 @@
+/**
+ * @file Integer.hpp
+ * @author 青羽 (chen_qingyu@qq.com, https://chen-qingyu.github.io/)
+ * @brief Big integer class.
+ * @version 0.1
+ * @date 2023.01.21
+ *
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef INTEGER_HPP
+#define INTEGER_HPP
+
+namespace mdspp
+{
+
+/**
+ * @brief Big integer class.
+ */
+class Integer
+{
+private:
+public:
+};
+
+} // namespace mdspp
+
+#endif // INTEGER_HPP
