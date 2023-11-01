bcce7cd9880d85cfd998dbeb587e40fa6477e5ba
chen-qingyu <236521827@qq.com>
2023-11-01 15:29:06 +0800

    添加mds.hpp注释和版本检查

--- Sources/mds.hpp
+++ Sources/mds.hpp
@@ -1,5 +1,17 @@
+/**
+ * @file mds.hpp
+ * @author 青羽 (chen_qingyu@qq.com, https://chen-qingyu.github.io/)
+ * @brief Unified header file.
+ * @version 1.0
+ * @date 2023.11.01
+ *
+ * @copyright Copyright (c) 2023
+ */
+
 #ifndef MDS_HPP
 #define MDS_HPP
+
+#if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
 
 #include "Deque.hpp"
 #include "Integer.hpp"
@@ -8,5 +20,7 @@
 #include "Set.hpp"
 #include "String.hpp"
 #include "Tuple.hpp"
+
+#endif // C++17
 
 #endif // MDS_HPP
