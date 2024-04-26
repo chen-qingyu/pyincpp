db182bbfa32b18f758733601087d951390aea40c
chen-qingyu <236521827@qq.com>
2024-04-26 18:42:08 +0800

    🌈 style: use rustyle doc-comments

--- sources/Integer.hpp
+++ sources/Integer.hpp
@@ -1,24 +1,22 @@
-/**
- * @file Integer.hpp
- * @author 青羽 (chen_qingyu@qq.com, https://chen-qingyu.github.io/)
- * @brief Integer class.
- * @date 2023.01.21
- *
- * @copyright Copyright (C) 2023
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+//! @file Integer.hpp
+//! @author Qingyu Chen (chen_qingyu@qq.com, https://chen-qingyu.github.io/)
+//! @brief Integer class.
+//! @date 2023.01.21
+//!
+//! @copyright Copyright (C) 2023 - 2024
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef INTEGER_HPP
 #define INTEGER_HPP
--- sources/pyincpp.hpp
+++ sources/pyincpp.hpp
@@ -1,24 +1,22 @@
-/**
- * @file pyincpp.hpp
- * @author 青羽 (chen_qingyu@qq.com, https://chen-qingyu.github.io/)
- * @brief Unified header file.
- * @date 2023.11.01
- *
- * @copyright Copyright (C) 2023
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+//! @file pyincpp.hpp
+//! @author Qingyu Chen (chen_qingyu@qq.com, https://chen-qingyu.github.io/)
+//! @brief Unified header file.
+//! @date 2023.11.01
+//!
+//! @copyright Copyright (C) 2023 - 2024
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef PYINCPP_HPP
 #define PYINCPP_HPP
