a6d73ee7a4f3ca6ca77a6e614a4128cffce2e3dd
chen-qingyu <236521827@qq.com>
2024-05-15 11:21:43 +0800

    🧪 test(benches): reduce return, adjust bench, make result more readable

--- benches/pyincpp_int_vs_other_int.cpp
+++ benches/pyincpp_int_vs_other_int.cpp
@@ -16,147 +16,147 @@ TEST_CASE("pyincpp::Int vs BigInt", "[int]")
     BigInt bint2(int2);
 
     REQUIRE(pyincpp::Int(int1) == "987654321000987654321000987654321000987654321000");
-    BENCHMARK("pyincpp::Int ctor")
+    BENCHMARK("construct (pyincpp)")
     {
         return pyincpp::Int(int1);
     };
 
     REQUIRE(BigInt(int1) == "987654321000987654321000987654321000987654321000");
-    BENCHMARK("BigInt ctor")
+    BENCHMARK("construct (BigInt)")
     {
         return BigInt(int1);
     };
 
     REQUIRE(pint1 + pint2 == "1111111110001111111110001111111110001111111110000");
-    BENCHMARK("pyincpp::Int +")
+    BENCHMARK("+ (pyincpp)")
     {
         return pint1 + pint2;
     };
 
     REQUIRE(bint1 + bint2 == "1111111110001111111110001111111110001111111110000");
-    BENCHMARK("BigInt +")
+    BENCHMARK("+ (BigInt)")
     {
         return bint1 + bint2;
     };
 
     REQUIRE(pint1 - pint2 == "864197532000864197532000864197532000864197532000");
-    BENCHMARK("pyincpp::Int -")
+    BENCHMARK("- (pyincpp)")
     {
         return pint1 - pint2;
     };
 
     REQUIRE(bint1 - bint2 == "864197532000864197532000864197532000864197532000");
-    BENCHMARK("BigInt -")
+    BENCHMARK("- (BigInt)")
     {
         return bint1 - bint2;
     };
 
     REQUIRE(pint1 * pint2 == "121932631112879134262225636335893338393537524450906873893338149672262225392470631112635269000000");
-    BENCHMARK("pyincpp::Int *")
+    BENCHMARK("* (pyincpp)")
     {
         return pint1 * pint2;
     };
 
     REQUIRE(bint1 * bint2 == "121932631112879134262225636335893338393537524450906873893338149672262225392470631112635269000000");
-    BENCHMARK("BigInt *")
+    BENCHMARK("* (BigInt)")
     {
         return bint1 * bint2;
     };
 
     REQUIRE(pint1 / pint2 == "8");
-    BENCHMARK("pyincpp::Int /")
+    BENCHMARK("/ (pyincpp)")
     {
         return pint1 / pint2;
     };
 
     REQUIRE(bint1 / bint2 == "8");
-    BENCHMARK("BigInt /")
+    BENCHMARK("/ (BigInt)")
     {
         return bint1 / bint2;
     };
 
     REQUIRE(pint1 % pint2 == "9000000000009000000000009000000000009000");
-    BENCHMARK("pyincpp::Int %")
+    BENCHMARK("% (pyincpp)")
     {
         return pint1 % pint2;
     };
 
     REQUIRE(bint1 % bint2 == "9000000000009000000000009000000000009000");
-    BENCHMARK("BigInt %")
+    BENCHMARK("% (BigInt)")
     {
         return bint1 % bint2;
     };
 
     auto pcopy = pyincpp::Int(pint1);
     REQUIRE(++pcopy == "987654321000987654321000987654321000987654321001");
-    BENCHMARK("pyincpp::Int ++")
+    BENCHMARK("++ (pyincpp)")
     {
         return ++pcopy;
     };
 
     auto bcopy = BigInt(bint1);
     REQUIRE(++bcopy == "987654321000987654321000987654321000987654321001");
-    BENCHMARK("BigInt ++")
+    BENCHMARK("++ (BigInt)")
     {
         return ++bcopy;
     };
 
     REQUIRE(pint1 > pint2);
-    BENCHMARK("pyincpp::Int >")
+    BENCHMARK("> (pyincpp)")
     {
         return pint1 > pint2;
     };
 
     REQUIRE(bint1 > bint2);
-    BENCHMARK("BigInt >")
+    BENCHMARK("> (BigInt)")
     {
         return bint1 > bint2;
     };
 
     REQUIRE(pyincpp::Int::gcd(pint1, pint2) == "9000000000009000000000009000000000009000");
-    BENCHMARK("pyincpp::Int gcd")
+    BENCHMARK("gcd (pyincpp)")
     {
         return pyincpp::Int::gcd(pint1, pint2);
     };
 
     REQUIRE(gcd(bint1, bint2) == "9000000000009000000000009000000000009000");
-    BENCHMARK("BigInt gcd")
+    BENCHMARK("gcd (BigInt)")
     {
         return gcd(bint1, bint2);
     };
 
     REQUIRE(pyincpp::Int::pow(pint1, 10) == "883180926273197021952264207133587213024757297231319710324392613444786407476937172097269547752144587748034859385950214986131578524631947404097159758634059531453759956655784361184056650195934688971795597131494125041289325199580638365345730937031303573757092905409889931409326947349999958418006278835787126126617733545641071231406892102718589117039785666092599334122903993297569621489728460437976845055240356743070703495190012103572275987661566940511201000000000000000000000000000000");
-    BENCHMARK("pyincpp::Int pow")
+    BENCHMARK("pow (pyincpp)")
     {
         return pyincpp::Int::pow(pint1, 10);
     };
 
     REQUIRE(pow(bint1, 10) == "883180926273197021952264207133587213024757297231319710324392613444786407476937172097269547752144587748034859385950214986131578524631947404097159758634059531453759956655784361184056650195934688971795597131494125041289325199580638365345730937031303573757092905409889931409326947349999958418006278835787126126617733545641071231406892102718589117039785666092599334122903993297569621489728460437976845055240356743070703495190012103572275987661566940511201000000000000000000000000000000");
-    BENCHMARK("BigInt pow")
+    BENCHMARK("pow (BigInt)")
     {
         return pow(bint1, 10);
     };
 
     REQUIRE(pyincpp::Int::sqrt(pint1) == "993807990006614735669893");
-    BENCHMARK("pyincpp::Int sqrt")
+    BENCHMARK("sqrt (pyincpp)")
     {
         return pyincpp::Int::sqrt(pint1);
     };
 
     REQUIRE(sqrt(bint1) == "993807990006614735669893");
-    BENCHMARK("BigInt sqrt")
+    BENCHMARK("sqrt (BigInt)")
     {
         return sqrt(bint1);
     };
 
     REQUIRE(pyincpp::Int::random(1000).digits() == 1000);
-    BENCHMARK("pyincpp::Int random")
+    BENCHMARK("random (pyincpp)")
     {
         return pyincpp::Int::random(1000);
     };
 
     REQUIRE(big_random(1000).to_string().size() == 1000);
-    BENCHMARK("BigInt random")
+    BENCHMARK("random (BigInt)")
     {
         return big_random(1000);
     };
@@ -175,31 +175,30 @@ benches\pyincpp_int_vs_other_int.cpp(7)
 
 benchmark name                            samples    iterations          mean
 -------------------------------------------------------------------------------
-pyincpp::Int ctor                              100           139    141.813 ns
-BigInt ctor                                    100            82    247.866 ns
-pyincpp::Int +                                 100            43    536.395 ns
-BigInt +                                       100             6    3.40883 us
-pyincpp::Int -                                 100            64    269.844 ns
-BigInt -                                       100             5       3.66 us
-pyincpp::Int *                                 100             3      7.614 us
-BigInt *                                       100             1    122.567 us
-pyincpp::Int /                                 100             9    2.58344 us
-BigInt /                                       100             1     36.993 us
-pyincpp::Int %                                 100             9    2.52556 us
-BigInt %                                       100             1     78.301 us
-pyincpp::Int ++                                100           276    76.8768 ns
-BigInt ++                                      100             6    3.40133 us
-pyincpp::Int >                                 100          5812    3.58723 ns
-BigInt >                                       100          1864    11.2827 ns
-pyincpp::Int gcd                               100             2    13.8315 us
-BigInt gcd                                     100             1    250.251 us
-pyincpp::Int pow                               100             1    767.197 us
-BigInt pow                                     100             1    4.08365 ms
-pyincpp::Int sqrt                              100             1    799.926 us
-BigInt sqrt                                    100             1    2.99672 ms
-pyincpp::Int random                            100             4    6.43075 us
-BigInt random                                  100             3    9.93367 us
+construct (pyincpp)                            100           142    142.859 ns
+construct (BigInt)                             100            80    252.975 ns
++ (pyincpp)                                    100            46    447.435 ns
++ (BigInt)                                     100             5     4.8318 us
+- (pyincpp)                                    100            76    280.447 ns
+- (BigInt)                                     100             5     4.2616 us
+* (pyincpp)                                    100             3    7.59633 us
+* (BigInt)                                     100             1    121.847 us
+/ (pyincpp)                                    100             8     2.5945 us
+/ (BigInt)                                     100             1     44.597 us
+% (pyincpp)                                    100             8      2.523 us
+% (BigInt)                                     100             1     90.192 us
+++ (pyincpp)                                   100           267    75.6442 ns
+++ (BigInt)                                    100             5     4.1202 us
+> (pyincpp)                                    100          5567     3.6093 ns
+> (BigInt)                                     100          1678    12.5703 ns
+gcd (pyincpp)                                  100             2    13.2665 us
+gcd (BigInt)                                   100             1    288.573 us
+pow (pyincpp)                                  100             1    771.099 us
+pow (BigInt)                                   100             1    4.19652 ms
+sqrt (pyincpp)                                 100             1    819.069 us
+sqrt (BigInt)                                  100             1    2.90994 ms
+random (pyincpp)                               100             4      6.336 us
+random (BigInt)                                100             3    9.62133 us
 
 ===============================================================================
-All tests passed (24 assertions in 1 test case)
 */
--- benches/std_vs_pyincpp.cpp
+++ benches/std_vs_pyincpp.cpp
@@ -16,39 +16,39 @@ TEST_CASE("std::vector vs pyincpp::List", "[std]")
     auto p = pyincpp::List<int>{1, 2, 3, 4, 5};
 
     REQUIRE(std::vector<int>{1, 2, 3, 4, 5} == s);
-    BENCHMARK("std::vector constructor")
+    BENCHMARK("construct (std)")
     {
         return std::vector<int>{1, 2, 3, 4, 5};
     };
 
     REQUIRE(pyincpp::List<int>{1, 2, 3, 4, 5} == p);
-    BENCHMARK("pyincpp::List constructor")
+    BENCHMARK("construct (pyincpp)")
     {
         return pyincpp::List<int>{1, 2, 3, 4, 5};
     };
 
     s.push_back(6);
     REQUIRE(s == std::vector<int>{1, 2, 3, 4, 5, 6});
-    BENCHMARK("std::vector append")
+    BENCHMARK("append (std)")
     {
         s.push_back(6);
     };
 
     p += 6;
     REQUIRE(p == pyincpp::List<int>{1, 2, 3, 4, 5, 6});
-    BENCHMARK("pyincpp::List append")
+    BENCHMARK("append (pyincpp)")
     {
         p += 6;
     };
 
     REQUIRE(s.at(0) == 1);
-    BENCHMARK("std::vector access with check")
+    BENCHMARK("access with check (std)")
     {
         return s.at(0);
     };
 
     REQUIRE(p[0] == 1);
-    BENCHMARK("pyincpp::List access with check")
+    BENCHMARK("access with check (pyincpp)")
     {
         return p[0];
     };
@@ -60,37 +60,39 @@ TEST_CASE("std::set vs pyincpp::Set", "[std]")
     auto p = pyincpp::Set<int>{1, 2, 3, 4, 5};
 
     REQUIRE(std::set<int>{1, 2, 3, 4, 5} == s);
-    BENCHMARK("std::set constructor")
+    BENCHMARK("construct (std)")
     {
         return std::set<int>{1, 2, 3, 4, 5};
     };
 
     REQUIRE(pyincpp::Set<int>{1, 2, 3, 4, 5} == p);
-    BENCHMARK("pyincpp::Set constructor")
+    BENCHMARK("construct (pyincpp)")
     {
         return pyincpp::Set<int>{1, 2, 3, 4, 5};
     };
 
-    REQUIRE(s.insert(6).second == true);
-    BENCHMARK("std::set add and check")
+    s.insert(6);
+    REQUIRE(s == std::set<int>{1, 2, 3, 4, 5, 6});
+    BENCHMARK("add (std)")
     {
-        return s.insert(6).second;
+        s.insert(6);
     };
 
-    REQUIRE(p.add(6) == true);
-    BENCHMARK("pyincpp::Set add and check")
+    p.add(6);
+    REQUIRE(p == pyincpp::Set<int>{1, 2, 3, 4, 5, 6});
+    BENCHMARK("add (pyincpp)")
     {
-        return p.add(6);
+        p.add(6);
     };
 
     REQUIRE(*s.find(2) == 2);
-    BENCHMARK("std::set find")
+    BENCHMARK("find (std)")
     {
         return *s.find(2);
     };
 
     REQUIRE(*p.find(2) == 2);
-    BENCHMARK("pyincpp::Set find")
+    BENCHMARK("find (pyincpp)")
     {
         return *p.find(2);
     };
@@ -102,37 +104,39 @@ TEST_CASE("std::map vs pyincpp::Dict", "[std]")
     auto p = pyincpp::Dict<int, char>{{1, 'A'}, {2, 'B'}, {3, 'C'}, {4, 'D'}, {5, 'E'}};
 
     REQUIRE(std::map<int, char>{{1, 'A'}, {2, 'B'}, {3, 'C'}, {4, 'D'}, {5, 'E'}} == s);
-    BENCHMARK("std::map constructor")
+    BENCHMARK("construct (std)")
     {
         return std::map<int, char>{{1, 'A'}, {2, 'B'}, {3, 'C'}, {4, 'D'}, {5, 'E'}};
     };
 
     REQUIRE(pyincpp::Dict<int, char>{{1, 'A'}, {2, 'B'}, {3, 'C'}, {4, 'D'}, {5, 'E'}} == p);
-    BENCHMARK("pyincpp::Dict constructor")
+    BENCHMARK("construct (pyincpp)")
     {
         return pyincpp::Dict<int, char>{{1, 'A'}, {2, 'B'}, {3, 'C'}, {4, 'D'}, {5, 'E'}};
     };
 
-    REQUIRE(s.insert({6, 'F'}).second == true);
-    BENCHMARK("std::map add and check")
+    s.insert({6, 'F'});
+    REQUIRE(s == std::map<int, char>{{1, 'A'}, {2, 'B'}, {3, 'C'}, {4, 'D'}, {5, 'E'}, {6, 'F'}});
+    BENCHMARK("add (std)")
     {
-        return s.insert({6, 'F'}).second;
+        s.insert({6, 'F'});
     };
 
-    REQUIRE(p.add(6, 'F') == true);
-    BENCHMARK("pyincpp::Dict add and check")
+    p.add(6, 'F');
+    REQUIRE(p == pyincpp::Dict<int, char>{{1, 'A'}, {2, 'B'}, {3, 'C'}, {4, 'D'}, {5, 'E'}, {6, 'F'}});
+    BENCHMARK("add (pyincpp)")
     {
-        return p.add(6, 'F');
+        p.add(6, 'F');
     };
 
     REQUIRE(s[5] == 'E');
-    BENCHMARK("std::map access")
+    BENCHMARK("access (std)")
     {
         return s[5];
     };
 
     REQUIRE(p[5] == 'E');
-    BENCHMARK("pyincpp::Dict access")
+    BENCHMARK("access (pyincpp)")
     {
         return p[5];
     };
@@ -144,37 +148,37 @@ TEST_CASE("std::string vs pyincpp::Str", "[std]")
     auto p = pyincpp::Str("hello world");
 
     REQUIRE(std::string("hello world") == s);
-    BENCHMARK("std::string constructor")
+    BENCHMARK("construct (std)")
     {
         return std::string("hello world");
     };
 
     REQUIRE(pyincpp::Str("hello world") == p);
-    BENCHMARK("pyincpp::Str constructor")
+    BENCHMARK("construct (pyincpp)")
     {
         return pyincpp::Str("hello world");
     };
 
     REQUIRE(s + "!" == "hello world!");
-    BENCHMARK("std::string append")
+    BENCHMARK("append (std)")
     {
         return s + "!";
     };
 
     REQUIRE(p + "!" == "hello world!");
-    BENCHMARK("pyincpp::Str append")
+    BENCHMARK("append (pyincpp)")
     {
         return p + "!";
     };
 
     REQUIRE(s.find("world") == 6);
-    BENCHMARK("std::string find")
+    BENCHMARK("find (std)")
     {
         return s.find("world");
     };
 
     REQUIRE(p.find("world") == 6);
-    BENCHMARK("pyincpp::Str find")
+    BENCHMARK("find (pyincpp)")
     {
         return p.find("world");
     };
@@ -186,37 +190,37 @@ TEST_CASE("std::tuple vs pyincpp::Tuple", "[std]")
     auto p = pyincpp::make_tuple(1, 2.33, 'A');
 
     REQUIRE(std::make_tuple(1, 2.33, 'A') == s);
-    BENCHMARK("std::tuple make_tuple")
+    BENCHMARK("make_tuple (std)")
     {
         return std::make_tuple(1, 2.33, 'A');
     };
 
     REQUIRE(pyincpp::make_tuple(1, 2.33, 'A') == p);
-    BENCHMARK("pyincpp::Tuple make_tuple")
+    BENCHMARK("make_tuple (pyincpp)")
     {
         return pyincpp::make_tuple(1, 2.33, 'A');
     };
 
     REQUIRE(s == s);
-    BENCHMARK("std::tuple compare")
+    BENCHMARK("compare (std)")
     {
         return s == s;
     };
 
     REQUIRE(p == p);
-    BENCHMARK("pyincpp::Tuple compare")
+    BENCHMARK("compare (pyincpp)")
     {
         return p == p;
     };
 
     REQUIRE(std::get<0>(s) == 1);
-    BENCHMARK("std::tuple get")
+    BENCHMARK("get (std)")
     {
         return std::get<0>(s);
     };
 
     REQUIRE(p.get<0>() == 1);
-    BENCHMARK("pyincpp::Tuple get")
+    BENCHMARK("get (pyincpp)")
     {
         return p.get<0>();
     };
@@ -228,41 +232,41 @@ TEST_CASE("std::deque vs pyincpp::Deque", "[std]")
     auto p = pyincpp::Deque<int>{1, 2, 3, 4, 5};
 
     REQUIRE(std::deque<int>{1, 2, 3, 4, 5} == s);
-    BENCHMARK("std::deque constructor")
+    BENCHMARK("construct (std)")
     {
         return std::deque<int>{1, 2, 3, 4, 5};
     };
 
     REQUIRE(pyincpp::Deque<int>{1, 2, 3, 4, 5} == p);
-    BENCHMARK("pyincpp::Deque constructor")
+    BENCHMARK("construct (pyincpp)")
     {
         return pyincpp::Deque<int>{1, 2, 3, 4, 5};
     };
 
     s.push_back(6);
     REQUIRE(s.back() == 6);
-    BENCHMARK("std::deque push_back")
+    BENCHMARK("push_back (std)")
     {
-        return s.push_back(6);
+        s.push_back(6);
     };
 
     p.push_back(6);
     REQUIRE(p.back() == 6);
-    BENCHMARK("pyincpp::Deque push_back")
+    BENCHMARK("push_back (pyincpp)")
     {
-        return p.push_back(6);
+        p.push_back(6);
     };
 
     s.push_front(0);
     REQUIRE(s.front() == 0);
-    BENCHMARK("std::deque push_front")
+    BENCHMARK("push_front (std)")
     {
         s.push_front(0);
     };
 
     p.push_front(0);
     REQUIRE(p.front() == 0);
-    BENCHMARK("pyincpp::Deque push_front")
+    BENCHMARK("push_front (pyincpp)")
     {
         p.push_front(0);
     };
@@ -281,12 +285,12 @@ benches\std_vs_pyincpp.cpp(13)
 
 benchmark name                            samples    iterations          mean
 -------------------------------------------------------------------------------
-std::vector constructor                        100           310    68.2903 ns
-pyincpp::List constructor                      100           303     68.868 ns
-std::vector append                             100          2700    2.64741 ns
-pyincpp::List append                           100          2351     2.8843 ns
-std::vector access with check                  100         22765   0.948649 ns
-pyincpp::List access with check                100         18637    1.13103 ns
+construct (std)                                100           293    74.4744 ns
+construct (pyincpp)                            100           290    70.1034 ns
+append (std)                                   100          2967     2.7489 ns
+append (pyincpp)                               100          2506    2.94852 ns
+access with check (std)                        100         21853   0.934929 ns
+access with check (pyincpp)                    100         17922    2.76599 ns
 
 -------------------------------------------------------------------------------
 std::set vs pyincpp::Set
@@ -296,73 +300,72 @@ benches\std_vs_pyincpp.cpp(57)
 
 benchmark name                            samples    iterations          mean
 -------------------------------------------------------------------------------
-std::set constructor                           100            49    425.735 ns
-pyincpp::Set constructor                       100            49    434.694 ns
-std::set add and check                         100          2839    6.81825 ns
-pyincpp::Set add and check                     100          3240    6.75247 ns
-std::set find                                  100          6081    3.35076 ns
-pyincpp::Set find                              100          6192     3.3876 ns
+construct (std)                                100            47    428.426 ns
+construct (pyincpp)                            100            47    428.064 ns
+add (std)                                      100          3098    6.45513 ns
+add (pyincpp)                                  100          3075    6.28325 ns
+find (std)                                     100          5878    3.25876 ns
+find (pyincpp)                                 100          5840    3.41473 ns
 
 -------------------------------------------------------------------------------
 std::map vs pyincpp::Dict
 -------------------------------------------------------------------------------
-benches\std_vs_pyincpp.cpp(99)
+benches\std_vs_pyincpp.cpp(101)
 ...............................................................................
 
 benchmark name                            samples    iterations          mean
 -------------------------------------------------------------------------------
-std::map constructor                           100            45    446.067 ns
-pyincpp::Dict constructor                      100            43    464.186 ns
-std::map add and check                         100          2968    6.96799 ns
-pyincpp::Dict add and check                    100           324    62.3858 ns
-std::map access                                100          3446    5.66599 ns
-pyincpp::Dict access                           100          4215    5.42278 ns
+construct (std)                                100            46    429.391 ns
+construct (pyincpp)                            100            45    451.356 ns
+add (std)                                      100          3102    6.44262 ns
+add (pyincpp)                                  100           322    62.1584 ns
+access (std)                                   100          3536    5.65243 ns
+access (pyincpp)                               100          4482    4.69723 ns
 
 -------------------------------------------------------------------------------
 std::string vs pyincpp::Str
 -------------------------------------------------------------------------------
-benches\std_vs_pyincpp.cpp(141)
+benches\std_vs_pyincpp.cpp(145)
 ...............................................................................
 
 benchmark name                            samples    iterations          mean
 -------------------------------------------------------------------------------
-std::string constructor                        100          8335    2.38164 ns
-pyincpp::Str constructor                       100          8599    2.56297 ns
-std::string append                             100          2818     7.2885 ns
-pyincpp::Str append                            100          1328     15.808 ns
-std::string find                               100          4025    5.24571 ns
-pyincpp::Str find                              100          1285    15.7642 ns
+construct (std)                                100          8386    2.38135 ns
+construct (pyincpp)                            100          8409    2.37805 ns
+append (std)                                   100          2693      7.241 ns
+append (pyincpp)                               100          1317    16.0744 ns
+find (std)                                     100          3350     6.1591 ns
+find (pyincpp)                                 100          1173     16.451 ns
 
 -------------------------------------------------------------------------------
 std::tuple vs pyincpp::Tuple
 -------------------------------------------------------------------------------
-benches\std_vs_pyincpp.cpp(183)
+benches\std_vs_pyincpp.cpp(187)
 ...............................................................................
 
 benchmark name                            samples    iterations          mean
 -------------------------------------------------------------------------------
-std::tuple make_tuple                          100         35033   0.594639 ns
-pyincpp::Tuple make_tuple                      100         34955   0.610556 ns
-std::tuple compare                             100         35051   0.598927 ns
-pyincpp::Tuple compare                         100         34986    0.59601 ns
-std::tuple get                                 100         33877   0.613366 ns
-pyincpp::Tuple get                             100         33709   0.614554 ns
+make_tuple (std)                               100         33502   0.594562 ns
+make_tuple (pyincpp)                           100         33623   0.626803 ns
+compare (std)                                  100         33664   0.594522 ns
+compare (pyincpp)                              100         33627   0.594195 ns
+get (std)                                      100         32451   0.611568 ns
+get (pyincpp)                                  100         32469   0.612122 ns
 
 -------------------------------------------------------------------------------
 std::deque vs pyincpp::Deque
 -------------------------------------------------------------------------------
-benches\std_vs_pyincpp.cpp(225)
+benches\std_vs_pyincpp.cpp(229)
 ...............................................................................
 
 benchmark name                            samples    iterations          mean
 -------------------------------------------------------------------------------
-std::deque constructor                         100            76    266.224 ns
-pyincpp::Deque constructor                     100            79    266.873 ns
-std::deque push_back                           100          1102     17.167 ns
-pyincpp::Deque push_back                       100          1034    17.7776 ns
-std::deque push_front                          100           929    17.5748 ns
-pyincpp::Deque push_front                      100           938    17.9797 ns
+construct (std)                                100            74      272.5 ns
+construct (pyincpp)                            100            74     270.77 ns
+push_back (std)                                100          1056    17.4356 ns
+push_back (pyincpp)                            100          1066    17.2129 ns
+push_front (std)                               100           954    16.8574 ns
+push_front (pyincpp)                           100           907     17.215 ns
 
 ===============================================================================
-All tests passed (36 assertions in 6 test cases)
 */
